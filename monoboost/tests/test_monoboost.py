32ee1fb2fdf6dd64e0e272c5156a3db141649c27
chriswbartley <chris@empirikalsystems.com>
2017-12-06 12:59:44 +0800

    AV

--- monoboost/tests/test_monoboost.py
+++ monoboost/tests/test_monoboost.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import, division, print_function
 import os.path as op
 import numpy as np
-import pandas as pd
+#import pandas as pd
 import numpy.testing as npt
-import monoboost as mb
+#import monoboost as mb
 
 #data_path = op.join(mb.__path__[0], 'data')
 
