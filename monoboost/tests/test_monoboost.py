6857f611470dfef0ee90e757620a4a8ad8218fb6
chriswbartley <chris@empirikalsystems.com>
2017-12-07 10:09:33 +0800

    TV

--- monoboost/tests/test_monoboost.py
+++ monoboost/tests/test_monoboost.py
@@ -1,11 +1,11 @@
 from __future__ import absolute_import, division, print_function
 import os.path as op
-import numpy as np
-#import pandas as pd
+# import numpy as np
+# import pandas as pd
 import numpy.testing as npt
 import monoboost as mb
 
-#data_path = op.join(mb.__path__[0], 'data')
+data_path = op.join(mb.__path__[0], 'data')
 
 
 def test_transform_data():
